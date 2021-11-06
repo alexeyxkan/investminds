70537b240349503c96958e0fb87a52183959bcb8
Alexey Kan <alexey@MacBook-Pro-Alexey.local>
2021-11-06 22:14:59 +0600

    Edited bot. Added gold/copper

--- bot/data.py
+++ bot/data.py
@@ -1,19 +1,32 @@
 import investpy
+import date
 
 
 def get_xfl():
     return investpy.get_etf_historical_data(etf='Financial Select Sector SPDR', country='United States',
-                                            from_date='16/10/2011',
-                                            to_date='16/10/2021')
+                                            from_date=date.ten_years_ago_str(),
+                                            to_date=date.today_str())
 
 
 def get_spx():
     return investpy.get_index_historical_data(index='S&P 500', country='United States',
-                                          from_date='16/10/2011',
-                                          to_date='16/10/2021')
+                                          from_date=date.ten_years_ago_str(),
+                                          to_date=date.today_str())
+
+                                    
+def get_copper():
+    return investpy.get_commodity_historical_data(commodity='Copper',
+                                                  from_date=date.ten_years_ago_str(), 
+                                                  to_date=date.today_str())
+
+
+def get_gold():
+    return investpy.get_commodity_historical_data(commodity='Gold',
+                                                  from_date=date.ten_years_ago_str(), 
+                                                  to_date=date.today_str())
 
 
 def get_us10yt():
     return investpy.get_bond_historical_data(bond='U.S. 10Y',
-                                             from_date='16/10/2011',
-                                             to_date='16/10/2021')
+                                             from_date=date.ten_years_ago_str(),
+                                             to_date=date.today_str())
--- bot/graph.py
+++ bot/graph.py
@@ -4,21 +4,37 @@ import pandas as pd
 import data
 
 
-def correlation_xfl_spx_to_us10yt():
-    plt.style.use('seaborn-whitegrid')
-    data1 = pd.DataFrame(data.get_xfl())["Close"] / pd.DataFrame(data.get_spx())["Close"]
-    data2 = data.get_us10yt()["Close"]
-    
-    plt.style.use('seaborn-whitegrid')
+def a_divide_b_and_c(a, b, c, name, label1, label2, filename):
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
     
-    data1.plot(x="Date", y="Close", ax=ax1, legend=False, color="g")
-    data2.plot(x="Date", y="Close", ax=ax2, legend=False, color="b")
+    (a / b).plot(x="Date", y="Close", ax=ax1, legend=False, color="g")
+    c.plot(x="Date", y="Close", ax=ax2, legend=False, color="b")
 
-    ax1.set_xlabel('Корреляция отношения Финансового сектора к S&P 500 и ставки по 10-летним tresuries')
-    ax1.set_ylabel('XFL/S&P_500', color='g')
-    ax2.set_ylabel('United States 10-Year Bond Yield', color='b')
+    ax1.set_xlabel(name)
+    ax1.set_ylabel(label1, color='g')
+    ax2.set_ylabel(label2, color='b')
 
-    fig.savefig('images/correlation_xfl_spx_to_us10yt.png')
+    fig.savefig(filename)
     plt.close()
+    
+    return fig
+
+
+def correlation_xfl_spx_to_us10yt():
+    a_divide_b_and_c(pd.DataFrame(data.get_xfl())["Close"],
+                     pd.DataFrame(data.get_spx())["Close"],
+                     data.get_us10yt()["Close"],
+                     'Корреляция отношения Финансового сектора к S&P 500 и ставки по 10-летним tresuries',
+                     'XFL/S&P_500', 'United States 10-Year Bond Yield',
+                     'images/correlation_xfl_spx_to_us10yt.png')
+    
+    
+def correlation_copper_gold_to_us10yt():
+    a_divide_b_and_c(pd.DataFrame(data.get_copper())["Close"], 
+                     pd.DataFrame(data.get_gold())["Close"],
+                     data.get_us10yt()["Close"],
+                     'Корреляция отношения меди к золоту и стаки по 10-летним tresuries',
+                     'Медь/Золото',
+                     'United States 10-Year Bond Yield',
+                     'images/correlation_copper_gold_to_us10yt.png')
